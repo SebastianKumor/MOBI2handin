d472f9960adb8d9e5ef189d31e18de6dca982495
Lubo Klucka <lubo.klucka@gmail.com>
2015-10-30 11:29:30 +0100

    arduino - read char as int

--- Arduino_LED/Arduino_LED.ino
+++ Arduino_LED/Arduino_LED.ino
@@ -1,26 +1,16 @@
-char INBYTE;
 int LED=13;
 int var=0;
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(LED,OUTPUT);
-Serial.println("start");
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+  Serial.println("start");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Serial.println("press one to run Arduino pin 13 LED On or zero to run it OFF");
   while(!Serial.available());
   
-  var = Serial.read();
-  Serial.print("I received: ");
-  Serial.println(var);
-  //if(var == 65)analogWrite(LED, 255);
- // if(var == 175)analogWrite(LED, var);
+  var = Serial.read() - '0';
   analogWrite(LED, var);
   delay(50);
-  // Serial.println(""+INBYTE);
-
 }
